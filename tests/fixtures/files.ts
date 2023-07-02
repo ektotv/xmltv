b7f1fff7cedd3192ee581680d22bd7b43cbfd561
Liam Potter <liam@liampotter.co.uk>
2023-07-02 18:05:26 +0100

    track the files fixture

---
+++ tests/fixtures/files.ts
@@ -0,0 +1,1 @@
+const files = ["c0-p0.xml","c0-p1.xml","c1-p0.xml","c1-p1.xml","c100-p100.xml","c100-p1000.xml","c100-p10000.xml","c100-p100000.xml","c100-p250000.xml","c100-p500000.xml","c100-p1000000.xml",]; export { files }
